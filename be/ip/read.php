631fab207e6bddec93ea718bfe5349a819035e9e
Simone Celia <simone.celia@eng.it>
2025-03-01 03:39:52 +0100

    1.0.0

---
+++ be/ip/read.php
@@ -0,0 +1,22 @@
+<?php
+include_once '../config/database.php';
+include_once '../models/Ip.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$ip_ = new Ip($db);
+$stmt = $ip_->read();
+$num = $stmt->rowCount();
+
+if ($num > 0) {
+	$row = $stmt->fetch(PDO::FETCH_ASSOC);
+	extract($row);
+	echo "$ip";
+	http_response_code(200);
+} else {
+	http_response_code(404);
+	echo json_encode(
+		array("message" => "No IP found.")
+	);
+}
